969325260a5cfbeff5e76733fbfa3535869a1d7e
Novimatrem <TheNovimatrem@protonmail.ch>
2025-04-15 08:00:18 +0100

    3waysplit-1050p-xfce.sh

---
+++ 3waysplit-1050p-xfce.sh
@@ -0,0 +1,25 @@
+# 3waysplit-1050p-xfce.sh
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+sleep 0.1s
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+wmctrl -r Steam -b add
+wmctrl -r Steam -e 0,530,0,306,1026
+wmctrl -r Steam -b add
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+wmctrl -r YouTube -b add
+wmctrl -r YouTube -e 0,-26,0,581,1080
+wmctrl -r YouTube -b add
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+wmctrl -r Discord -b add
+wmctrl -r Discord -e 0,811,0,894,1076
+wmctrl -r Discord -b add
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+clear && echo "3waysplit-1050p-xfce.sh" && echo "" && echo "Ensure you have Steam (small mode), and two Firefoxes (Discord, and YouTube)    open for this script to work."
+echo ""
+echo "Done."
+
+
+# EOF
+
